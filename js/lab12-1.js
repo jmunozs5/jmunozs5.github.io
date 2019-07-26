7619311c8a9bf594452d881886aeeae4ada04a85
jmunozs5 <jonathanm2596@gmail.com>
2019-07-26 12:41:17 -0600

    commit

--- js/lab12-1.js
+++ js/lab12-1.js
@@ -4,14 +4,13 @@ Processing: obtain the first and last variable of the array
 Output: addition of the first and last variable.
 */
 function addEnds(list) {
-    list = [10, 20, 30, 40, 50 ,60];
-      let firstv = list[0];
-      let secondv = list[list.length - 1];
-      let sum = firstv + secondv;
-    
-    
-    
+  list = [10, 20, 30, 40, 50 ,60];
+    let firstv = list[0];
+    let secondv = list[list.length - 1];
+    let sum = firstv + secondv
       
-      document.getElementById("array").innerHTML = list;
-  }
+  
+    document.getElementById("output").innerHTML = sum;
+    document.getElementById("array").innerHTML = list;
+}
   
